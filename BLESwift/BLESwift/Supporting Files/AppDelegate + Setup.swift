e86fc27857db7219669eeda93c75f3bd47c96b25
Grafonsky <d3cembry@gmail.com>
2024-04-25 03:29:44 +0600

    init project

---
+++ BLESwift/BLESwift/Supporting Files/AppDelegate + Setup.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate + Setup.swift
+//  BLESwift
+//
+//  Created by Bohdan Hawrylyshyn on 25.04.24.
+//
+
+import Foundation
+
+extension AppDelegate {
+    internal func setupDependencies() {
+        
+    }
+}
